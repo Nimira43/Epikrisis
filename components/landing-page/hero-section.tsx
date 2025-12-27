f22acf1886919a3ac34307e9f8d912bc0367802f
Nimira43 <n.rabone72@aol.com>
2025-12-27 23:03:41 +0000

    Live-Badge -4

--- components/landing-page/hero-section.tsx
+++ components/landing-page/hero-section.tsx
@@ -8,9 +8,10 @@ const LiveBadge = () => {
       className='px-4 py-2 mb-8 text-sm backdrop-blur-sm'
     >
       <span className='relative flex h-2 w-2'>
-        <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75'></span>
+        <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-primary opacity-75' />
+        <span className='relative inline-flex rounded-full h-2 w-2 bg-primary' />
       </span>
-      Become a part of the Epikrisis Community
+      <span className='text-muted-foreground ml-2'>Become a part of the Epikrisis Community!</span>
     </Badge>
   )
 }
