a233b21e8594c8c3082e19247605c8cf38d4ce82
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:48:45 +0000

    More styling

--- components/landing-page/hero-section.tsx
+++ components/landing-page/hero-section.tsx
@@ -5,13 +5,14 @@ export default function HeroSection() {
   return (
     <section className='relative overflow-hidden bg-gray-300'>
       <div className='wrapper'>
-        <Badge>Become a part of the Epikrisis Community</Badge>
-        <h1>Empowering Developers to Share and Showcase Their Creations</h1>
-        <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
-        <Button>Share</Button>
-        <Button variant='outline'>Explore</Button>
+        <div className='flex flex-col items-center justify-center lg:py-24 py-12 space-y-6 text-center'>
+          <Badge>Become a part of the Epikrisis Community</Badge>
+          <h1>Empowering Developers to Share and Showcase Their Creations</h1>
+          <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
+          <Button>Share</Button>
+          <Button variant='outline'>Explore</Button>
+        </div>   
       </div>
-      
     </section>
   )
 }
