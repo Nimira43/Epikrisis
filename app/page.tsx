94dd4d67e2e61f072500abb3538d1b8fe468f0cd
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:15:32 +0000

    Create main button variant

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div>
       <h1>Empowering Developers to Share and Showcase Their Creations</h1>
       <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
+      <Button variant='main'>Share</Button>
     </div>
     
   )
