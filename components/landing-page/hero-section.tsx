3e6ffd1d236451116a4f3da22b7d84683905f98c
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:54:25 +0000

    Live-Badge 2

--- components/landing-page/hero-section.tsx
+++ components/landing-page/hero-section.tsx
@@ -2,12 +2,19 @@ import { Badge } from '../ui/badge'
 import { Button } from '../ui/button'
 
 const LiveBadge = () => {
-  return <Badge>Become a part of the Epikrisis Community</Badge>
+  return (
+    <Badge
+      variant='outline'
+      className='px-4 py-2 mb-8 text-sm backdrop-blur-sm'
+    >
+      Become a part of the Epikrisis Community
+    </Badge>
+  )
 }
 
 export default function HeroSection() {
   return (
-    <section className='relative overflow-hidden bg-gray-300'>
+    <section className='relative overflow-hidden bg-linear-to-b from-background via-background to-muted/20'>
       <div className='wrapper'>
         <div className='flex flex-col items-center justify-center lg:py-24 py-12 space-y-6 text-center'>
           <LiveBadge />
