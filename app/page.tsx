b65286d60b405792973ff497b59d15ce5c92d5a4
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:11:11 +0000

    Hero heading and paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
   return (
-    <>
-      <div>Home</div>
-      <Button variant=''>Login</Button>
-    </>
+    <div>
+      <h1>Empowering Developers to Share and Showcase Their Creations</h1>
+      <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
+    </div>
     
   )
 }
