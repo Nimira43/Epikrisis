92d7c20ef6d499718580f5017c7444824b9cb639
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:44:55 +0000

    Added wrapper

--- components/landing-page/hero-section.tsx
+++ components/landing-page/hero-section.tsx
@@ -4,12 +4,14 @@ import { Button } from '../ui/button'
 export default function HeroSection() {
   return (
     <section className='relative overflow-hidden bg-gray-300'>
-      <div></div>
-      <Badge>Become a part of the Epikrisis Community</Badge>
-      <h1>Empowering Developers to Share and Showcase Their Creations</h1>
-      <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
-      <Button>Share</Button>
-      <Button variant='outline'>Explore</Button>
+      <div className='wrapper'>
+        <Badge>Become a part of the Epikrisis Community</Badge>
+        <h1>Empowering Developers to Share and Showcase Their Creations</h1>
+        <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
+        <Button>Share</Button>
+        <Button variant='outline'>Explore</Button>
+      </div>
+      
     </section>
   )
 }
