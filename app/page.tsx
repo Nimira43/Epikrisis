99c52039956cb7b312fcf4f8c7bd0cc9c2836873
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:30:57 +0000

    Updating Badge

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <div>
-      <span>Become a part of the Epikrisis Community</span>
+      <Badge>Become a part of the Epikrisis Community</Badge>
       <h1>Empowering Developers to Share and Showcase Their Creations</h1>
       <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
       <Button>Share</Button>
