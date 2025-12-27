4f8d337b904c2b9f6f2be409bd056e03eee6ff12
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:49:59 +0000

    LiveBadge

--- components/landing-page/hero-section.tsx
+++ components/landing-page/hero-section.tsx
@@ -1,12 +1,16 @@
 import { Badge } from '../ui/badge'
 import { Button } from '../ui/button'
 
+const LiveBadge = () => {
+  return <Badge>Become a part of the Epikrisis Community</Badge>
+}
+
 export default function HeroSection() {
   return (
     <section className='relative overflow-hidden bg-gray-300'>
       <div className='wrapper'>
         <div className='flex flex-col items-center justify-center lg:py-24 py-12 space-y-6 text-center'>
-          <Badge>Become a part of the Epikrisis Community</Badge>
+          <LiveBadge />
           <h1>Empowering Developers to Share and Showcase Their Creations</h1>
           <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
           <Button>Share</Button>
