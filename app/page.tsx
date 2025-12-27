690beeaf8ee5b31821519b951c43fd7394441278
Nimira43 <n.rabone72@aol.com>
2025-12-27 22:37:16 +0000

    Moved content of Home into HeroSection and brought HeroSection into Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
+import HeroSection from '@/components/landing-page/hero-section'
 
 export default function Home() {
   return (
     <div>
-      <Badge>Become a part of the Epikrisis Community</Badge>
-      <h1>Empowering Developers to Share and Showcase Their Creations</h1>
-      <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
-      <Button>Share</Button>
-      <Button variant='outline'>Explore</Button>
+      <HeroSection />
     </div>
     
   )
---
+++ components/landing-page/hero-section.tsx
@@ -0,0 +1,14 @@
+import { Badge } from '../ui/badge'
+import { Button } from '../ui/button'
+
+export default function HeroSection() {
+  return (
+    <div>
+      <Badge>Become a part of the Epikrisis Community</Badge>
+      <h1>Empowering Developers to Share and Showcase Their Creations</h1>
+      <p>Submit your coding concepts, refine them with community insight, and watch your project progress from proposal to approved entry in Buddy’s growing inventory.</p>
+      <Button>Share</Button>
+      <Button variant='outline'>Explore</Button>
+    </div>
+  )
+}
