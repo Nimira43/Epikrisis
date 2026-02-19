f70bfe813b61dc844dfb3f33cd04854f3567a3d7
Nimira43 <n.rabone72@aol.com>
2026-02-19 04:34:24 +0000

    hasVoted buttons.

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card'
 import { Badge } from '../ui/badge'
+import { Button } from '../ui/button'
+import { IoChevronDownOutline, IoChevronUpOutline } from 'react-icons/io5'
+import { cn } from '@/lib/utils'
 
 interface Project {
   id: number
@@ -15,10 +18,12 @@ export default function ProjectCard({
   project
 }: {
   project: Project
-}) {
+  }) {
+  const hasVoted = false
+
   return (
     <Link href={`/projects/${project.id}`}>
-      <Card className='group card-hover hover:bg-primary-foreground/10 border-solid border-gray-400 min-h-45'>
+      <Card className='group card-hover hover:bg-primary-foreground/40 border-solid border-gray-400 min-h-45'>
         <CardHeader className='flex-1'>
           <div className='flex items-start gap-4'>
             <div className='flex-1 min-w-0'>
@@ -35,6 +40,35 @@ export default function ProjectCard({
               <CardDescription>
                 {project.description}
               </CardDescription>
+            </div>
+            <div className='flex flex-col items-center gap-1 shrink-0'>
+              <Button
+                variant='outline'
+                size='icon-sm'
+                className={cn(
+                  'h-8 w-8 text-primary',
+                  hasVoted
+                    ? 'hover:border-green-500'
+                    : 'hover:primary cursor-not-allowed'
+                )}
+              >
+                <IoChevronUpOutline className='size-5' />
+              </Button>
+              <span className='text-sm font-semibold transition-colors text-foreground'
+              >
+                10
+              </span>
+              <Button
+                variant='outline'
+                size='icon-sm'
+                className={cn(
+                  'h-8 w-8 text-primary',
+                  hasVoted
+                    ? 'hover:border-green-500'
+                    : 'hover:primary cursor-not-allowed')}
+              >
+                <IoChevronDownOutline className='size-5' />
+              </Button>
             </div>
             
           </div>  
